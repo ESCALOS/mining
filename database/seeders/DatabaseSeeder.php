30b012e8b4c2a476c9f3fc935b49cc2696255edf
Escalos <stornblood6969@gmail.com>
2023-04-12 11:37:37 -0500

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use \App\Models\User;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,19 +16,40 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $user = User::factory()->create([
-             'name' => 'Carlos Escate Roman',
-             'email' => 'stornblood6969@gmail.com',
+
+        $user = User::create([
+            'name' => 'Carlos Escate Roman',
+            'email' => 'stornblood6969@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
         ]);
-        $user2 = User::factory()->create([
+        $user2 = User::create([
             'name' => 'Administrador',
             'email' => 'prinmig70@gmail.com',
-            'password' => 'Clari',
-       ]);
-       $user3 = User::factory()->create([
-           'name' => 'Rubén Tipiani',
-           'email' => 'rtipiani@gmail.com',
-      ]);
+            'email_verified_at' => now(),
+            'password' => bcrypt('Clari'), // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
+        $user3 = User::create([
+            'name' => 'Rubén Tipiani',
+            'email' => 'rtipiani@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
         $role = \Spatie\Permission\Models\Role::create(['name' => 'administrador']);
         $user->assignRole($role);
         $user2->assignRole($role);
